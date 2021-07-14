e9c309fdd3c28a05f8826b49abe621bbb5722c86
liu <86836576+liuthis@users.noreply.github.com>
2021-07-14 11:18:16 +0800

    Add files via upload

---
+++ js/index.js
@@ -0,0 +1,36 @@
+searchClick();
+// randomback();
+	getback();
+window.onload = function(){
+	isBack();
+    juzi();
+    date();
+    search();
+    displayWindowSize();
+    engine();
+    backFilter();
+
+}
+window.onresize = function(){
+    displayWindowSize();
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    
